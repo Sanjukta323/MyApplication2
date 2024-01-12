d3adc4f86e68e7fec4ba9d0dff0fce8bc4f752d7
Sanjukta Giri <sanjuktagiri2021@gmail.com>
2024-01-12 17:06:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mylibrary.Toasty;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toasty.simpleToast(this,"Hello Toast");
+    }
+
+
+}
